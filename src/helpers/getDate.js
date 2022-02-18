a1aca8189eade13ba96645e1306b488f70eeb496
Marcos <emmanuel.m.lopez@gmail.com>
2022-02-17 22:01:09 -0300

    added some descriptions

--- src/helpers/getDate.js
+++ src/helpers/getDate.js
@@ -1,5 +1,6 @@
 import moment from 'moment';
 
+/* HELPER TO GET DATE (DAY, NUMBER, MONTH, YEAR) */
 const getDate = (timezone) => {
   const timeZoneInMinutes = timezone / 60;
   const day = moment().utcOffset(timeZoneInMinutes).format("dddd D MMMM YYYY")
--- src/helpers/getTime.js
+++ src/helpers/getTime.js
@@ -1,5 +1,6 @@
 import moment from "moment";
 
+/* HELPER TO GET TIME ON CITY (HOUR, MINUTES, PM OR ARM) */
 const getTime = (timezone) => {
   const timezoneInMinutes = timezone / 60;
   const currTime = moment().utcOffset(timezoneInMinutes).format("h:mm A");
